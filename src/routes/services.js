988d62ca2776e478ff08f08c1f463105897dff7e
i-guerrero <isaacguerrero@pursuit.org>
2023-04-09 10:17:03 -0400

    Create accounts and services routes and refactor views to account for new routes

---
+++ src/routes/services.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+const { accounts, writeJSON } = require("../data");
+
+router.get("/transfer", (req, res) => {
+  res.render("transfer");
+});
+
+router.post("/transfer", (req, res) => {
+  let { from, to, amount } = req.body;
+  accounts[from].balance -= amount;
+  accounts[to].balance += parseInt(amount, 10);
+
+  writeJSON();
+
+  res.render("transfer", { message: "Transfer Completed" });
+});
+
+router.get("/payment", (req, res) => {
+  res.render("payment", { account: accounts.credit });
+});
+
+router.post("/payment", (req, res) => {
+  accounts.credit.balance -= req.body.amount;
+  accounts.credit.available += parseInt(req.body.amount, 10);
+
+  writeJSON();
+
+  res.render("payment", {
+    message: "Payment Successful",
+    account: accounts.credit,
+  });
+});
+
+module.exports = router;
